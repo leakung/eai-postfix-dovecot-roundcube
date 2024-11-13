9f5093b0c1de81d184ce430b6b06e55daadae110
leakung <leakung@gmail.com>
2024-11-13 19:05:40 +0000

    roundcube 1.6.9 no need to modify

--- fix-roundcube.sh
+++ fix-roundcube.sh
@@ -29,13 +29,16 @@ cat <<EOF >> data/www/config/config.inc.php
     ];
 EOF
 
+# roundcube 1.6.9 support EAI
+# no need to modify
+
 # fix roundcube for support EAI
 # roundcube 1.6.7
 
-sed -i 's/\.replace(\/\[\^a\-z\]$\/gi\,\"\")/.replace(\/[^ก-๛a-z]$\/gi,"")/g' data/www/skins/elastic/ui.min.js
+#sed -i 's/\.replace(\/\[\^a\-z\]$\/gi\,\"\")/.replace(\/[^ก-๛a-z]$\/gi,"")/g' data/www/skins/elastic/ui.min.js
 
-sed -i 's/idn_to_ascii/idn_to_utf8/g' data/www/program/include/rcmail_sendmail.php
-sed -i 's/check_email($item)/check_email(rcube_utils::idn_to_ascii($item))/g' data/www/program/include/rcmail_sendmail.php
+#sed -i 's/idn_to_ascii/idn_to_utf8/g' data/www/program/include/rcmail_sendmail.php
+#sed -i 's/check_email($item)/check_email(rcube_utils::idn_to_ascii($item))/g' data/www/program/include/rcmail_sendmail.php
 
-sed -i 's/idn_to_ascii/idn_to_utf8/g' data/www/program/actions/settings/identity_save.php
-sed -i 's/check_email($email)/check_email(rcube_utils::idn_to_ascii($email))/g' data/www/program/actions/settings/identity_save.php
+#sed -i 's/idn_to_ascii/idn_to_utf8/g' data/www/program/actions/settings/identity_save.php
+#sed -i 's/check_email($email)/check_email(rcube_utils::idn_to_ascii($email))/g' data/www/program/actions/settings/identity_save.php
